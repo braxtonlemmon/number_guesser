94e2f9e4a88ca5b03edb47c1b09ac17ccce286c9
Braxton Lemmon <braxtonlemmon@gmail.com>
2019-06-11 20:51:20 +0200

    I5: Add cheat mode.

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,25 +1,62 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = rand(101)
+@@count = 5
+@@number = rand(101)
+cheat = ""
+guess = nil
+background = 'white'
+message = "Guess a number between 0 and 100!"
+result = ""
 
-def check_guess(guess, number)
-	guess = guess.to_i
-	if guess == number
+get '/' do
+	if params['cheat'] == 'true'
+		cheat = "I don't know why you want to see this... Secret number: #{@@number}"
+	end
+
+	if params['guess'] != nil
+		result = ""
+		guess = params["guess"].to_i
+		message = check_guess(guess)
+		background = set_background(message)
+		@@count -= 1
+		if guess == @@number || @@count == 0
+			result = (guess == @@number) ? "Congratulations you won!" : "Sorry you lost..."
+			result << " The game will now be reset."
+			@@count = 5
+			@@number = rand(101)
+			cheat = ""
+			params['cheat'] = false
+		end	
+	end
+	erb :index, :locals => {:number => @@number, 
+													:message => message,
+													:count => @@count,
+													:result => result,
+													:cheat => cheat,
+													:background => background
+												 }
+end
+
+def check_guess(guess)
+	if guess == @@number
 		"You got it right!"
-	elsif guess - number > 5
-		"Way too high!"
-	elsif guess - number < -5
-		"Way too low!"
-	elsif guess > number
-		"Too high!"
-	elsif guess < number
-		"Too low!"
+	elsif guess > @@number
+		(guess > @@number + 5) ? "Way too high!" : "Too high!"
+	elsif guess < @@number
+		(guess < @@number - 5) ? "Way too low!" : "Too low!"
+	else "Error!"
 	end
 end
 
-get '/' do
-	guess = params["guess"]
-	message = check_guess(guess, number)
-	erb :index, :locals => {:number => number, :message => message }
+def set_background(message)
+	case message
+	when "You got it right!" then 'green'
+	when "Way too high!"     then 'red'
+	when "Way too low!"      then 'red'
+	when "Too high!"         then 'pink'
+	when "Too low!"          then 'pink'
+	else "white"
+	end
 end
+
